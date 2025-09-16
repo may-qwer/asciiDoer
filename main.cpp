957619538937b7dfecd3610aa6d673c5d42050c7
may-qwer <may0220@yandex.ru>
2025-09-16 20:33:03 +0300

    todo: search libs for work with pictures

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// #include "/all/cpp/libs/opencv/include/opencv2/opencv.hpp"
+#include "opencv/include/opencv2/opencv.hpp"
+// #include </all/cpp/libs/opencv/include/opencv2/opencv.hpp>
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
